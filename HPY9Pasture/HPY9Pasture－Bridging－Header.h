96d12b24d0b2362487e6785583722bf1bd08aa05
purepure0 <purepure@815785047deimac.local>
2016-10-20 10:25:54 +0800

    0000

---
+++ HPY9Pasture/HPY9Pasture－Bridging－Header.h
@@ -0,0 +1,13 @@
+//
+//  HPY9Pasture－Bridging－Header.h
+//  HPY9Pasture
+//
+//  Created by purepure on 16/10/20.
+//  Copyright © 2016年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+#ifndef HPY9Pasture_Bridging_Header_h
+#define HPY9Pasture_Bridging_Header_h
+
+//#import <AliyunPlayerSDK/AliVcMediaPlayer.h>
+#endif /* HPY9Pasture_Bridging_Header_h */
